bff5d89c04f6cdf544bab5a71e1d6ca7be39bd0f
ChristopherOrtiz69 <christopher.villafanet@gmail.com>
2023-08-31 11:28:42 -0600

    final Score

--- Assets/_Chris/Scripts/TimeAccountChris.cs
+++ Assets/_Chris/Scripts/TimeAccountChris.cs
@@ -7,15 +7,20 @@ public class TimeAccountChris : MonoBehaviour
 {
     public TMP_Text timetext;
     public  float time;
+    public GameObject canvasPuntuacion;
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
         DisplayTime(time);
+       /* if (canvasPuntuacion(false))
+        {
+            canvasPuntuacion.SetActive(true);
+        }*/
     }
 
-    void DisplayTime(float TimetoDisplay)
+     public void DisplayTime(float TimetoDisplay)
     {
         if (TimetoDisplay < 0)
         {
@@ -25,10 +30,16 @@ public class TimeAccountChris : MonoBehaviour
         {
             TimetoDisplay += 1;
         }
-
+       
         float minutes = Mathf.FloorToInt(TimetoDisplay / 60);
         float seconds = Mathf.FloorToInt(TimetoDisplay % 60);
+        /*if (seconds == 2)
+        {
+            Debug.Log("GameOver");
+        }*/
 
         timetext.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
+
+
 }
--- Assets/_Chris/Scripts/finalScore.cs
+++ Assets/_Chris/Scripts/finalScore.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class finalScore : MonoBehaviour
 {
+    
+    public TMP_Text timeText;
+    public float time;
+    public GameObject panelPuntuacion;
+
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Puntuacion();
+    }
+    // Update is called once per frame
+    private void Puntuacion ()
+    {
+        if (Comparisons.CartasVolteadas >= 12)
+        {
+            Debug.Log("DETECTE ERROR");
+            panelPuntuacion.SetActive(true);
+            Debug.Log("Active Panel");
+        }
+            
+
+    }
+
+    public void finalTime (TimeAccountChris finalScore)
+    {
+        float time = finalScore.time; 
     }
 }
