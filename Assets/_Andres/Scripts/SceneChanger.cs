07f54126875dc2ce2dea1a67f3d539c3377910d8
970510-star <71453690+970510-star@users.noreply.github.com>
2023-09-08 13:17:12 -0500

    Cambio de escenas

--- Assets/_Andres/Scripts/SceneChanger.cs
+++ Assets/_Andres/Scripts/SceneChanger.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SceneLoader(int SceneIndex)
     {
-        
+        SceneManager.LoadScene(SceneIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Exit()
     {
-        
+        Application.Quit();
     }
 }
