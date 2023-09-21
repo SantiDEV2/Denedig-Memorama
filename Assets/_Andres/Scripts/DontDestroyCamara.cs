6c0554932e5118cb33b1a88f993b2e541fbf326c
Santiago MR <100314893+SantiDEV2@users.noreply.github.com>
2023-09-21 11:36:57 -0600

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 9dcbc1f186e090cf369e8e83c7674399f962d01b, reversing
    changes made to 0d1c550d0faa2b42a820e59bd0710fe48d325f46.

--- Assets/_Andres/Scripts/DontDestroyCamara.cs
+++ Assets/_Andres/Scripts/DontDestroyCamara.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DontDestroyDrop : MonoBehaviour
+public class DontDestroyCamara : MonoBehaviour
 {
     void Awake()
     {
