a90960d729d64d6327c8d448ad80946ea31dab73
ChristopherOrtiz69 <christopher.villafanet@gmail.com>
2023-10-23 09:25:50 -0600

    bug

--- Assets/_Max/Scripts/aparecerdesaparecerboton.cs
+++ Assets/_Max/Scripts/aparecerdesaparecerboton.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+<<<<<<< Updated upstream
 using UnityEngine.UI;
+=======
+>>>>>>> Stashed changes
 
 public class aparecerdesaparecerboton : Comparisons
 {
     public GameObject boton;
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD:Assets/_Max/Scripts/aparecerydesaparecerboton.cs
+    public float tiempoEspera = 0.5f;
+
+   
+    private bool botonActivo = false;
+=======
+>>>>>>> Stashed changes
     //public Transform cartas;
 
     // Start is called before the first frame update
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> fba8f5073f83968a0839ae4e307d4b3624db2966:Assets/_Max/Scripts/aparecerdesaparecerboton.cs
+>>>>>>> Stashed changes
 
     void Start()
     {
         boton.SetActive(false);
+<<<<<<< Updated upstream
         //boton.onClick.AddListener(OcultarBoton);
     }
 
@@ -57,4 +74,62 @@ public class aparecerdesaparecerboton : Comparisons
     }
 
     
+=======
+    }
+
+<<<<<<< HEAD:Assets/_Max/Scripts/aparecerydesaparecerboton.cs
+    private void Update()
+=======
+    // Update is called once per frame
+
+    /*bool CartasNoEnPosicionInicial()
+>>>>>>> fba8f5073f83968a0839ae4e307d4b3624db2966:Assets/_Max/Scripts/aparecerdesaparecerboton.cs
+    {
+        logicaOleadas();
+    }
+
+    private void logicaOleadas()
+    {
+        if (Cuentalugar == 5 )
+        {
+            boton.SetActive(true);
+            botonActivo = true;
+            Debug.Log("Estoy prendiendo el botón");
+            if (boton == true)
+            {
+                boton.SetActive(false);
+            }
+        }
+
+        if (Cuentalugar == 9)
+        {
+            Debug.Log("Estoy prendiendo el botón 2");
+        }
+<<<<<<< HEAD:Assets/_Max/Scripts/aparecerydesaparecerboton.cs
+    }
+
+    public void DesactivarBoton()
+    {
+        boton.SetActive(false);
+        botonActivo = false;
+        Debug.Log("Estoy apagando el botón");
+    }
+=======
+        if (Cuentalugar == 12)
+        {
+            boton.gameObject.SetActive(true);
+        }
+        if (Cuentalugar == 16)
+        {
+            boton.gameObject.SetActive(true);
+        }
+        if (Cuentalugar == 20)
+        {
+            boton.gameObject.SetActive(true);
+        }
+>>>>>>> fba8f5073f83968a0839ae4e307d4b3624db2966:Assets/_Max/Scripts/aparecerdesaparecerboton.cs
+
+
+    }
+>>>>>>> Stashed changes
 }
