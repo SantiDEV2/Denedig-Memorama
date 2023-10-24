d4b24c0d2e8814442a1d192a458121a5e9e4665b
Santiago MR <100314893+SantiDEV2@users.noreply.github.com>
2023-10-24 10:51:20 -0600

    UI Changes

--- Assets/_Ana/Scripts/TextSizeAndFontChanger.cs
+++ Assets/_Ana/Scripts/TextSizeAndFontChanger.cs
@@ -6,13 +6,12 @@ using TMPro;
 
 public class TextSizeAndFontChanger : MonoBehaviour
 {
-    public List<TextMeshProUGUI> textMeshProTexts; // Arrastra los objetos de texto TextMeshPro aquí en el Inspector
-    public float newSize = 20f; // Tamaño del texto que deseas establecer al hacer clic en el botón
-    public TMP_FontAsset newFont; // Fuente que deseas establecer al hacer clic en el botón
+    public List<TextMeshProUGUI> textMeshProTexts; // Arrastra los objetos de texto TextMeshPro aquï¿½ en el Inspector
+    public float newSize = 20f; // Tamaï¿½o del texto que deseas establecer al hacer clic en el botï¿½n
 
     private void Start()
     {
-        // Asegúrate de que al menos un objeto TextMeshPro esté asignado en el Inspector
+        // Asegï¿½rate de que al menos un objeto TextMeshPro estï¿½ asignado en el Inspector
         if (textMeshProTexts == null || textMeshProTexts.Count == 0)
         {
             Debug.LogError("No se han asignado objetos TextMeshPro en la lista en el Inspector.");
@@ -21,14 +20,13 @@ public class TextSizeAndFontChanger : MonoBehaviour
 
     public void ChangeTextSizeAndFont()
     {
-        // Itera a través de la lista de objetos TextMeshPro y cambia su tamaño y fuente
+        // Itera a travï¿½s de la lista de objetos TextMeshPro y cambia su tamaï¿½o y fuente
         foreach (var textMeshProText in textMeshProTexts)
         {
-            // Cambia el tamaño del texto TextMeshPro al valor especificado
+            // Cambia el tamaï¿½o del texto TextMeshPro al valor especificado
             textMeshProText.fontSize = newSize;
 
             // Cambia la fuente del texto TextMeshPro al valor especificado
-            textMeshProText.font = newFont;
         }
     }
 }
